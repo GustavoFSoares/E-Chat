3468e15e1b3fdafd27f51ad47a101b65c1f6cdf8
Gustavo Soares <gustavo.soares@f1solucoes.com.br>
2017-02-01 16:49:25 -0200

    criando função para adicionar usuario no banco --> classe usuarioDAO

---
+++ class/usuarioDAO.php
@@ -0,0 +1,18 @@
+<?php
+  class usuarioDAO{
+
+    private $conexao;
+
+      function __construct($conexao){
+        $this->conexao = $conexao;
+      }
+
+      function adicionaUsuario($nome){
+        $query="INSERT INTO usuario (nome) VALUES ('{$nome}')";
+        $resultado = mysqli_query($this->conexao, $query);
+
+        return $resultado;
+      }
+
+
+  }
