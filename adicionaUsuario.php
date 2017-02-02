1fdfe36f757d1f3b7996e05dd1d8b3b9b97f3b1a
Felipe Azevedo Juchem <felipe.juchem@f1solucoes.com.br>
2017-02-02 13:43:33 -0200

    removido alerta chato

--- adicionaUsuario.php
+++ adicionaUsuario.php
@@ -3,19 +3,18 @@
     require_once('logicaUsuario.php');
     require_once('class/UsuarioDAO.php');
 
-    var_dump($_POST);
     $nome = $_POST['nome'];
-
 
     $usuarioDAO = new usuarioDAO($conecta);
 
     if ($nome != null) {
 
         if ($usuarioDAO->adicionaUsuario($nome)) {
-          logarUsuario($nome); ?>
-          <script> alert("Usuario Adicionado!"); window.location.href="index.php";</script> <?php
+          logarUsuario($nome);
+          header("location: index.php");
+          ?>
         } else { ?>
-          <script> alert("Usuario não foi adicionado! /n Mude o seu nome!"); window.location.href="index.php";</script> <?php
+          
+          <?php
         }
-
-    }
+      }
