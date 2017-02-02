4d02e2cdb16b34439fc0e20000976a698d870d4a
Felipe Azevedo Juchem <felipe.juchem@f1solucoes.com.br>
2017-02-02 10:12:14 -0200

    add label

--- loginFormulario.php
+++ loginFormulario.php
@@ -1,16 +1,6 @@
-<table>
-<h1> Login </h1>
-
+<div id="form-login">
     <form action="adicionaUsuario.php" method="post">
-        <tr>
-          <td>Nome:</td>
-          <td><input type="text" name="nome"></td>
-        </tr>
-
-        <tr>
-          <td></td>
-          <td><button type="submit">Entrar</button></td>
-        </tr>
+          <input type="text" name="nome" placeholder="Digite seu nome">
+          <button type="submit">Entrar</button>
     </form>
-
-</table>
+</div>
