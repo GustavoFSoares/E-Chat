5a5f5c7be5e2ce7d521953e0077682266559cf75
Felipe Azevedo Juchem <felipe.juchem@f1solucoes.com.br>
2017-02-02 10:39:28 -0200

    cabecario implementado

---
+++ cabecalho.php
@@ -0,0 +1,20 @@
+<?php
+    require_once("logicaUsuario.php");
+?>
+
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="css/geral.css">
+        <link rel="stylesheet" type="text/css" href="css/loginFormulario.css">
+        <link rel="stylesheet" type="text/css" href="css/cabecalho.css">
+        <link href="https://fonts.googleapis.com/css?family=Comfortaa" rel="stylesheet">
+        <meta charset="utf-8">
+        <title>EChat</title>
+    </head>
+
+    <body>
+        <header>
+            <div id="nav">
+                <h1>E-Chat</h1>
+            </div>
+        </header>
